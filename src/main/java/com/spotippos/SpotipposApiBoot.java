6cc846e03fd3a5c85b4eb087eb332036c9a7ae62
Felipe Praça <tredezine@gmail.com>
2017-06-11 21:03:04 -0300

    Criando projeto

---
+++ src/main/java/com/spotippos/SpotipposApiBoot.java
@@ -0,0 +1,13 @@
+package com.spotippos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotipposApiBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotipposApiBoot.class, args);
+	}
+
+}
